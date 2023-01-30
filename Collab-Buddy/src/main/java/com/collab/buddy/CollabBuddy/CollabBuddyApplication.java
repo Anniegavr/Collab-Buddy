45262d8a50c0ead775f434606126c502fc0b9002
Anniegavr <56108881+Anniegavr@users.noreply.github.com>
2023-01-30 21:50:15 +0200

    add initial configuration
    - add liquibase configuration (with dummy migrations for testing purposes)
    - add datasource configuration
    - add logging configuration

---
+++ Collab-Buddy/src/main/java/com/collab/buddy/CollabBuddy/CollabBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.collab.buddy.CollabBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabBuddyApplication.class, args);
+	}
+
+}
---
+++ Collab-Buddy/src/test/java/com/collab/buddy/CollabBuddy/CollabBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab.buddy.CollabBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
