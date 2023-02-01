dddc6d7a40c9c8e1613613cc3db5ef6904b6ea90
agavrilita <agavrilita@inthergroup.com>
2023-02-01 12:49:03 +0200

    - Add SpringBoot Admin dependencies and configurations for server - Add SpringBoot Admin UI - Add SpringBoot Admin Client configurations

--- Collab-Buddy/src/main/java/com/collab/buddy/CollabBuddy/CollabBuddyApplication.java
+++ Collab-Buddy/src/main/java/com/collab/buddy/CollabBuddy/CollabBuddyApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CollabBuddyApplication {
+@EnableAdminServer
+public class CollabBuddyApplication(exclude = AdminServerHazelcastAutoConfiguration.class) {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollabBuddyApplication.class, args);
