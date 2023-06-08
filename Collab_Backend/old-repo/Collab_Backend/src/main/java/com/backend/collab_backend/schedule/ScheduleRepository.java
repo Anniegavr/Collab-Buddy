df32be25914eb2d27adfd6154fb8d155ca578b96
agavrilita <agavrilita@inthergroup.com>
2023-06-08 13:03:45 +0300

    - reset branch

--- Collab_Backend/old-repo/Collab_Backend/src/main/java/com/backend/collab_backend/schedule/ScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.collab_backend.schedule;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-}
